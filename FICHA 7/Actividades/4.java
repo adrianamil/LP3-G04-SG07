423795be132df3254a22bd6f209e6511fff90f92
BLAR-08 <134863620+BLAR-08@users.noreply.github.com>
2023-10-21 00:10:19 -0500

    Create 4.java

---
+++ FICHA 7/Actividades/4.java
@@ -0,0 +1,32 @@
+public class AppStack{
+    public static void main(String[] args) {
+        try {
+            metodo1();
+        }
+        catch (Exception exception) {
+            System.out.printf("%s\n\n", exception.getMessage());
+            exception.printStackTrace();
+
+            StackTraceElement[] itemRastreo = exception.getStackTrace();
+
+            System.out.println("\nRastreo de la pila de getStackTrace:");
+            System.out.println("Clase\t\t\tArchivo\t\tLinea\tMetodo");
+
+            for(StackTraceElement item : itemRastreo){
+                System.out.printf("%s\t", item.getClassName());
+                System.out.printf("%s\t", item.getFileName());
+                System.out.printf("%s\t", item.getLineNumber());
+                System.out.printf("%s\t", item.getMethodName());
+            }
+        }
+    }
+    public static void metodo1() throws Exception{
+        metodo2();
+    }
+    public static void metodo2() throws Exception{
+        metodo3();
+    }
+    public static void metodo3() throws Exception{
+        throw  new Exception("La excepcion se lanzo");
+    }
+}
