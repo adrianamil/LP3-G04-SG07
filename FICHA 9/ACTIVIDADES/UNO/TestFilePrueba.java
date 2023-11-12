bfa92291dad4d9e6f9571a9049a83ac7b49c3ea1
BLAR-08 <134863620+BLAR-08@users.noreply.github.com>
2023-11-11 19:22:06 -0500

    Add files via upload

---
+++ FICHA 9/ACTIVIDADES/UNO/TestFilePrueba.java
@@ -0,0 +1,30 @@
+package UNO;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import javax.swing.JFrame;
+
+
+public class TestFilePrueba {
+    public static void main(String[] args )throws IOException {
+        FileInputStream file;
+        byte b[] = new byte[1024];
+        try{
+            file = new FileInputStream("C:/Users/Ana Paula/OneDrive/UNIVERSIDAD/LP 3/PRACTICA/FICHA 9/ACTIVIDADES/UNO/TestFile.java");
+
+            file.read(b);
+            String s = new String(b);
+            ViewFile view = new ViewFile(s);
+            view.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            view.setSize(500, 150);
+            view.setVisible(true);  
+        }
+        catch (FileNotFoundException e){
+            System.out.println(e.getMessage());
+        }
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }  
+}
---
+++ FICHA 9/ACTIVIDADES/UNO/ViewFile.java
@@ -0,0 +1,14 @@
+package UNO;
+
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+
+public class ViewFile extends JFrame {
+    private final JTextArea areaTexto;
+   
+    public ViewFile(String s) {
+        super("Mostrando el contenido de un archivo");
+        areaTexto=new JTextArea(s,5, 40);
+        add(areaTexto);
+    }
+}
